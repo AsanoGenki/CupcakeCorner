82bbc17e6d9a03541b5e09523cc006c0c1ab5acb
AsanoGenki <asanogenki0218@icloud.com>
2024-08-21 14:39:37 +0900

    Initial commit.

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Genki on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
